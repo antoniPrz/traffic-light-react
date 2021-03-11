9f74613e5b6f6c1877159d8b78881b60d83b4f53
antoni perez <antoni.protoni@gmail.com>
2021-03-10 21:06:22 -0300

    light on off 1

--- src/App.js
+++ src/App.js
@@ -11,21 +11,63 @@ import React, {useState} from "react";
 
 
 
-function App() {
+// function App() {
 
 
-const [lightON ,setColor] = useState ("red");
-  return (
+// const [lightON ,setColor] = useState ("red");
+//   return (
 
-    <div>
+//     <div>
+     
+//   <div className="container">
+//   <div className="circle red" color="red" onClick={() => setColor("on") }> </div>
+//   <div className="circle yellow" color="yelow"></div>
+//   <div className="circle " color="green"></div>
+// </div>
+//     </div>
+//   );
+// }
+
+class Traffic extends React.Component{
+
+state = {
+
+  on: false
+}
+brillo = () => {
+  this.setState({on: !this.state.on})
+
+}
+render(){
+  if (this.state.on){
+    return(
+      <div>
      
       <div className="container">
-  <div className="circle red" color="red" onClick={() => setColor("on") }> </div>
-  <div className="circle yellow" color="yelow"></div>
-  <div className="circle green" color="green"></div>
-</div>
+      <div className={"circle red  "} onClick={this.brillo }> </div>
+      <div className={"circle  yellow  "} onClick={this.brillo}> </div>
+      <div className={"circle green  "} onClick={this.brillo}> </div>
     </div>
-  );
+        </div>
+    )
+    }
+    else{
+      return (
+        <div>
+     
+        <div className="container">
+        <div className={"circle red on "} onClick={this.brillo}> </div>
+        <div className={"circle yellow on "} onClick={this.brillo}> </div>
+        <div className={"circle green on "} onClick={this.brillo}> </div>
+      </div>
+          </div>
+      )
+    }
+
 }
 
-export default App;
+
+}
+
+export default Traffic;
+
